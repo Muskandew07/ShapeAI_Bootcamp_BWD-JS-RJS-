d29c08ff61d7e1af7378be6a9cb31cf2307f9d61
Muskandew07 <86553646+Muskandew07@users.noreply.github.com>
2021-06-27 16:09:23 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Submition</h1>
+      <p>
+        A basic web developmenet React js Bootcamp with lots of fun and great
+        experience.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
